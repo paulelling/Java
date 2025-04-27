f3ce8be8ae74396ad00675a5b86bad8db342a7ef
paulelling <paulelling@yahoo.com>
2025-04-27 18:56:53 -0500

    Update UserController.java

--- MachineProgramsAPI/src/main/java/org/mp/machineprogramsapi/api/controller/UserController.java
+++ MachineProgramsAPI/src/main/java/org/mp/machineprogramsapi/api/controller/UserController.java
@@ -3,6 +3,7 @@ package org.mp.machineprogramsapi.api.controller;
 import org.mp.machineprogramsapi.api.model.User;
 import org.mp.machineprogramsapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/user")
     public User getUser(@RequestParam Integer id) {
         Optional user = userService.getUser(id);
