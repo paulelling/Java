d03e71bcc5bf203967b0b06301eacf560ae1d051
paulelling <paulelling@yahoo.com>
2025-04-30 03:39:38 -0500

    Added JUnit

--- MachineProgramsAPI/src/test/java/org/mp/machineprogramsapi/MachineProgramsApiApplicationTests.java
+++ MachineProgramsAPI/src/test/java/org/mp/machineprogramsapi/MachineProgramsApiApplicationTests.java
@@ -1,7 +1,14 @@
 package org.mp.machineprogramsapi;
 
 import org.junit.jupiter.api.Test;
+import org.mp.machineprogramsapi.repository.Database;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MachineProgramsApiApplicationTests {
@@ -10,4 +17,15 @@ class MachineProgramsApiApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void addition() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void firstName() throws ParserConfigurationException, IOException, SAXException {
+        Database database = new Database();
+        String firstName = database.GetFirstName();
+        assertEquals("Kate", firstName);
+    }
 }
